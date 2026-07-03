9da602a5adefb0c285188dbc68bd2ca47f6a6b2a
poqq26566-cmyk <poqq26566@gmail.com>
2026-07-03 18:23:03 +0800

    Update main.dart

--- banana_split_flutter/lib/main.dart
+++ banana_split_flutter/lib/main.dart
@@ -1,12 +1,12 @@
 // lib/main.dart
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import 'package:banana_split_flutter/crypto/passphrase.dart';
+
 import 'package:banana_split_flutter/state/create_notifier.dart';
 import 'package:banana_split_flutter/state/restore_notifier.dart';
 import 'package:banana_split_flutter/screens/create_screen.dart';
